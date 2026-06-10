e3d57d1a0d77816f8f57d90aa527c45a461d719f
Akshatha M <akshatha_m@Akshathas-MacBook-Air.local>
2026-06-10 19:05:19 +0530

    change to backend tf file as per warning

--- backend.tf
+++ backend.tf
@@ -3,7 +3,7 @@ terraform {
         bucket = "terraform-iac-akshatha-2026"
         region = "us-west-2"
         key = "infra/terraform.tfstate"
-        dynamodb_table = "terraform-state-lock"
+        use_lockfile = "terraform-state-lock"
         encrypt = true
     }
 }
