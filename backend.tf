c18a97e2975aa5da96daf876d41f60a2953c8702
Akshatha M <akshatha_m@Akshathas-MacBook-Air.local>
2026-06-10 19:07:39 +0530

    back to previous version of dynamodb_table

--- backend.tf
+++ backend.tf
@@ -3,7 +3,7 @@ terraform {
         bucket = "terraform-iac-akshatha-2026"
         region = "us-west-2"
         key = "infra/terraform.tfstate"
-        use_lockfile = "terraform-state-lock"
+        dynamodb_table = "terraform-state-lock"
         encrypt = true
     }
 }
